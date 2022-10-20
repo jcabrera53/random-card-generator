25887e976da6c005d2231cbcde5e04ba9ca6d849
jcabrera53 <juancabrera53@gmail.com>
2022-10-20 18:46:12 +0000

    generador de cartas de poker con F5

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let symbol = ["♥", "♦", "♣", "♠"];
+
+  let randomNumber = number[Math.floor(Math.random() * number.length)];
+  let randomSymbol = symbol[Math.floor(Math.random() * symbol.length)];
+
+  document.getElementsByClassName("up-left")[0].innerHTML =
+    randomNumber + "<br>" + randomSymbol;
+  document.getElementsByClassName("medium")[0].innerHTML = randomSymbol;
+  document.getElementsByClassName("bottom-right")[0].innerHTML =
+    randomNumber + "<br>" + randomSymbol;
+
+  if (randomSymbol == "♥" || randomSymbol == "♦") {
+    document.getElementsByClassName("up-left")[0].style.color = "red";
+    document.getElementsByClassName("medium")[0].style.color = "red";
+    document.getElementsByClassName("bottom-right")[0].style.color = "red";
+  }
 };
